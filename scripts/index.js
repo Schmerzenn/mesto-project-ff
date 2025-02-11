a8693ba4a93297461dad34ea75cd73684bc159e2
Диана М <alisa.sokolova.2001@list.ru>
2025-02-11 13:03:12 +0300

    fix1

--- scripts/index.js
+++ scripts/index.js
@@ -8,22 +8,29 @@ function deleteCard(event) {
   }
 }
 
-function addElement(item, deleteCard) {
+function createCard(item, deleteCard) {
   const cardElement = elementTemplate.querySelector(".card").cloneNode(true); //клонирование
 
   cardElement.querySelector(".card__image").src = item.link;
   cardElement.querySelector(".card__image").alt = item.name;
   cardElement.querySelector(".card__title").textContent = item.name;
 
-  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
-
-  placesList.append(cardElement);
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button"); // тут создаете карточку и возвращаете ее
 
   cardDeleteButton.addEventListener("click", (e) => {
     deleteCard(e);
   });
+  return cardElement;
 }
 
-initialCards.forEach(function (item) {
-  addElement(item, deleteCard);
-});
+function addElements(initialCards, deleteCard) {
+  const fragment = document.createDocumentFragment();
+
+  initialCards.forEach((item) => {
+    fragment.appendChild(createCard(item, deleteCard));
+  });
+
+  placesList.appendChild(fragment);
+}
+
+addElements(initialCards, deleteCard);
